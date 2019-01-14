c6146ec50df08f2d7dd3b0963115445dd092be38
Hao Zhang <zhangh0214@gmail.com>
2019-01-14 08:38:56 +0800

    Add files via upload

--- data-loader/cub200.py
+++ data-loader/cub200.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*
-"""CUB200-2011 dataset for PyTorch loading.
+"""This module is served as torchvision.datasets to load CUB200-2011.
 
 CUB200-2011 dataset has 11,788 images of 200 bird species. The project page
 is as follows.
@@ -22,6 +22,14 @@ import pickle
 import numpy as np
 import PIL.Image
 import torch
+import torch.utils.data
+
+
+torch.set_default_dtype(torch.float32)
+torch.set_default_tensor_type(torch.FloatTensor)
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+torch.backends.cudnn.benchmark = True
 
 
 __all__ = ['CUB200', 'CUB200ReLU']
@@ -31,8 +39,8 @@ __date__ = '2018-01-09'
 __email__ = 'zhangh0214@gmail.com'
 __license__ = 'CC BY-SA 3.0'
 __status__ = 'Development'
-__updated__ = '2018-03-04'
-__version__ = '6.0'
+__updated__ = '2018-05-23'
+__version__ = '14.1'
 
 
 class CUB200(torch.utils.data.Dataset):
@@ -45,10 +53,10 @@ class CUB200(torch.utils.data.Dataset):
             and transforms it.
         _target_transform, callable: A function/transform that takes in the
             target and transforms it.
-        _train_data, list of np.ndarray.
-        _train_labels, list of int.
-        _test_data, list of np.ndarray.
-        _test_labels, list of int.
+        _train_instances, list<np.ndarray>.
+        _train_labels, list<int>.
+        _test_instances, list<np.ndarray>.
+        _test_labels, list<int>.
     """
     def __init__(self, root, train=True, transform=None, target_transform=None,
                  download=False):
@@ -69,72 +77,30 @@ class CUB200(torch.utils.data.Dataset):
         self._train = train
         self._transform = transform
         self._target_transform = target_transform
+        self.name = 'cub200'
 
-        if self._checkIntegrity():
-            print('Files already downloaded and verified.')
+        if (os.path.isfile(os.path.join(self._root, 'train.pkl'))
+            and os.path.isfile(os.path.join(self._root, 'test.pkl'))):
+            print('Dataset already downloaded and verified.')
         elif download:
             url = ('http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/'
                    'CUB_200_2011.tgz')
             self._download(url)
             self._extract()
         else:
-            raise RuntimeError(
-                'Dataset not found. You can use download=True to download it.')
+            self._extract()
 
         # Now load the picked data.
         if self._train:
-            self._train_data, self._train_labels = pickle.load(open(
-                os.path.join(self._root, 'processed/train.pkl'), 'rb'))
-            assert (len(self._train_data) == 5994
+            self._train_instances, self._train_labels = pickle.load(open(
+                os.path.join(self._root, 'train.pkl'), 'rb'))
+            assert (len(self._train_instances) == 5994
                     and len(self._train_labels) == 5994)
         else:
-            self._test_data, self._test_labels = pickle.load(open(
-                os.path.join(self._root, 'processed/test.pkl'), 'rb'))
-            assert (len(self._test_data) == 5794
+            self._test_instances, self._test_labels = pickle.load(open(
+                os.path.join(self._root, 'test.pkl'), 'rb'))
+            assert (len(self._test_instances) == 5794
                     and len(self._test_labels) == 5794)
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index, int: Index.
-
-        Returns:
-            image, PIL.Image: Image of the given index.
-            target, str: target of the given index.
-        """
-        if self._train:
-            image, target = self._train_data[index], self._train_labels[index]
-        else:
-            image, target = self._test_data[index], self._test_labels[index]
-        # Doing this so that it is consistent with all other datasets.
-        image = PIL.Image.fromarray(image)
-
-        if self._transform is not None:
-            image = self._transform(image)
-        if self._target_transform is not None:
-            target = self._target_transform(target)
-
-        return image, target
-
-    def __len__(self):
-        """Length of the dataset.
-
-        Returns:
-            length, int: Length of the dataset.
-        """
-        if self._train:
-            return len(self._train_data)
-        return len(self._test_data)
-
-    def _checkIntegrity(self):
-        """Check whether we have already processed the data.
-
-        Returns:
-            flag, bool: True if we have already processed the data.
-        """
-        return (
-            os.path.isfile(os.path.join(self._root, 'processed/train.pkl'))
-            and os.path.isfile(os.path.join(self._root, 'processed/test.pkl')))
 
     def _download(self, url):
         """Download and uncompress the tar.gz file from a given URL.
@@ -150,10 +116,10 @@ class CUB200(torch.utils.data.Dataset):
         if not os.path.isdir(raw_path):
             os.mkdir(raw_path, mode=0o775)
         if not os.path.isdir(processed_path):
-            os.mkdir(processed_path, mode=0x775)
+            os.mkdir(processed_path, mode=0o775)
 
         # Downloads file.
-        fpath = os.path.join(self._root, 'raw/CUB_200_2011.tgz')
+        fpath = os.path.join(self._root, 'raw', 'CUB_200_2011.tgz')
         try:
             print('Downloading ' + url + ' to ' + fpath)
             six.moves.urllib.request.urlretrieve(url, fpath)
@@ -174,17 +140,18 @@ class CUB200(torch.utils.data.Dataset):
 
     def _extract(self):
         """Prepare the data for train/test split and save onto disk."""
-        image_path = os.path.join(self._root, 'raw/CUB_200_2011/images/')
+        image_path = os.path.join(self._root, 'raw', 'CUB_200_2011', 'images')
         # Format of images.txt: <image_id> <image_name>
         id2name = np.genfromtxt(os.path.join(
-            self._root, 'raw/CUB_200_2011/images.txt'), dtype=str)
+            self._root, 'raw', 'CUB_200_2011', 'images.txt'), dtype=str)
         # Format of train_test_split.txt: <image_id> <is_training_image>
         id2train = np.genfromtxt(os.path.join(
-            self._root, 'raw/CUB_200_2011/train_test_split.txt'), dtype=int)
+            self._root, 'raw', 'CUB_200_2011', 'train_test_split.txt'),
+            dtype=int)
 
-        train_data = []
+        train_instances = []
         train_labels = []
-        test_data = []
+        test_instances = []
         test_labels = []
         for id_ in range(id2name.shape[0]):
             image = PIL.Image.open(os.path.join(image_path, id2name[id_, 1]))
@@ -197,16 +164,48 @@ class CUB200(torch.utils.data.Dataset):
             image.close()
 
             if id2train[id_, 1] == 1:
-                train_data.append(image_np)
+                train_instances.append(image_np)
                 train_labels.append(label)
             else:
-                test_data.append(image_np)
+                test_instances.append(image_np)
                 test_labels.append(label)
 
-        pickle.dump((train_data, train_labels),
-                    open(os.path.join(self._root, 'processed/train.pkl'), 'wb'))
-        pickle.dump((test_data, test_labels),
-                    open(os.path.join(self._root, 'processed/test.pkl'), 'wb'))
+        torch.save((train_instances, train_labels),
+                   os.path.join(self._root, 'processed', 'train.pth'))
+        torch.save((test_instances, test_labels),
+                   os.path.join(self._root, 'processed', 'test.pth'))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index, int: Index.
+
+        Returns:
+            instance, PIL.Image: Image of the given index.
+            label, int: target of the given index.
+        """
+        if self._train:
+            instance, label = (self._train_instances[index],
+                               self._train_labels[index])
+        else:
+            instance, label = (self._test_instances[index],
+                               self._test_labels[index])
+        instance = PIL.Image.fromarray(instance)
+        if self._transform is not None:
+            instance = self._transform(instance)
+        if self._target_transform is not None:
+            label = self._target_transform(label)
+        return instance, label
+
+    def __len__(self):
+        """Length of the dataset.
+
+        Returns:
+            length, int: Length of the dataset.
+        """
+        if self._train:
+            return len(self._train_instances)
+        return len(self._test_instances)
 
 
 class CUB200ReLU(torch.utils.data.Dataset):
@@ -230,22 +229,24 @@ class CUB200ReLU(torch.utils.data.Dataset):
         self._root = os.path.expanduser(root)  # Replace ~ by the complete dir
         self._train = train
 
-        if self._checkIntegrity():
+        if (os.path.isfile(os.path.join(self._root, 'relu5-3', 'train.pth'))
+            and os.path.isfile(os.path.join(self._root, 'relu5-3',
+                                            'test.pth'))):
             print('CUB200 relu5-3 features already prepared.')
         else:
             raise RuntimeError('CUB200 relu5-3 Dataset not found.'
-                'You need to prepare it in advance.')
+                               'You need to prepare it in advance.')
 
         # Now load the picked data.
         if self._train:
-            self._train_data, self._train_labels = torch.load(
+            self._train_instances, self._train_labels = torch.load(
                 os.path.join(self._root, 'relu5-3', 'train.pth'))
-            assert (len(self._train_data) == 5994
+            assert (len(self._train_instances) == 5994
                     and len(self._train_labels) == 5994)
         else:
-            self._test_data, self._test_labels = torch.load(
+            self._test_instances, self._test_labels = torch.load(
                 os.path.join(self._root, 'relu5-3', 'test.pth'))
-            assert (len(self._test_data) == 5794
+            assert (len(self._test_instances) == 5794
                     and len(self._test_labels) == 5794)
 
     def __getitem__(self, index):
@@ -258,8 +259,8 @@ class CUB200ReLU(torch.utils.data.Dataset):
             target, int: target of the given index.
         """
         if self._train:
-            return self._train_data[index], self._train_labels[index]
-        return self._test_data[index], self._test_labels[index]
+            return self._train_instances[index], self._train_labels[index]
+        return self._test_instances[index], self._test_labels[index]
 
     def __len__(self):
         """Length of the dataset.
@@ -268,15 +269,5 @@ class CUB200ReLU(torch.utils.data.Dataset):
             length, int: Length of the dataset.
         """
         if self._train:
-            return len(self._train_data)
-        return len(self._test_data)
-
-    def _checkIntegrity(self):
-        """Check whether we have already processed the data.
-
-        Returns:
-            flag, bool: True if we have already processed the data.
-        """
-        return (
-            os.path.isfile(os.path.join(self._root, 'relu5-3', 'train.pth'))
-            and os.path.isfile(os.path.join(self._root, 'relu5-3', 'test.pth')))
+            return len(self._train_instances)
+        return len(self._test_instances)
